9a62de17f48e045d03accb2aebf83b1de5341fc3
Javohir <javohirrahimjonov546@gmail.com>
2025-11-12 13:16:38 +0300

    Math js-4lesson

---
+++ js/app.js
@@ -0,0 +1,64 @@
+//========================
+//     1-massala
+//========================
+//prompt orqali 1-999 gacha son qabul qiling. Kiritgan son 1-999 oralig'ida bo'lmasa yoki boshqa narsa bo'lsa xatolik deb chiqaring.
+//  Aytaylik 365 kiritdi, siz esa konsolga uch yuz oltmish besh deb chiqarishingiz kerak.
+
+
+
+
+
+
+
+//////////////////////////////////////////
+//2-massala
+//////////////////////////////////////////
+//Siz 44dan 66gacha istalgan son o'ylaysiz, kompyuter topishi kerak.
+//  Agar to'g'ri topsa while to'xtashi kerak, agar topa olmasa qaytadan yangi sonni aytishi kerak.
+//  Darsdagi amaliyotga o'xshash.
+
+// while (true) {
+//   let min = 44;
+//   let max = 67;
+//   random = Math.trunc(Math.random() * (max - min) + min);
+
+//   let num = prompt("Oylagan soningiz shumi?:" + random);
+//   if (num === "ha") break;
+// }
+
+//=======================
+// Tushuntirish
+// let min va let max bunda faqat 44 va 67 ni orasidagi random sonlarni oladi math trunc butun sondan keyingi raqamlarni chopib tashlaydi
+//math random esa 44 va 67 orasidagi sonlarni random shakilda beradi. foydalanuvchi 44dan 66 gacha bolgan sonlardan
+//oylaydi va kompyuter random son beradi togri bolsa ha deydi va while break boladi va toxtaydi!
+
+
+
+
+//////////////////////////////////////////
+//3-massala
+/////////////////////////////////////////
+//promptdan so'z kiritadi, kiritgan so'zini siz harflab konsolga chiqarishingiz kerak. Misol uchun hello kiritdi.
+//Siz uni:
+
+//h
+
+//e
+
+//l
+
+//l
+
+//o
+
+// let w = prompt("so'z kiriting:");
+// s = w.split("").join("\n");
+// console.log(s);
+
+//=====================
+//Tushuntirish
+//Bu yerda "w" promptga kiritilgan soz shun ozgaruvchiga saqlanadi , "w.split"- bu kiritilgan sozni harflarga bo'ladi,
+//  ".join(\n)"-yangi qator yaratadi yani har bir harf uchun yangi qator ochadi
+//=====================
+
+
